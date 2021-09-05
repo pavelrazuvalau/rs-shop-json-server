7b2ff130290b4f6ad9fedceb81f981fc86918485
Pavel Razuvalau <pavel@razuvalov.com>
2021-09-05 12:51:01 +0300

    feat: add categories documentation

--- services/core/categories/categories.middleware.js
+++ services/core/categories/categories.middleware.js
@@ -2,6 +2,47 @@ const express = require('express');
 const router = express.Router();
 const url = require('url');
 
+
+/**
+  * @swagger
+  *
+  * tags:
+  *   name: categories
+  *   description: API for managing categories
+  *
+  * components:
+  *   schemas:
+  *     SubCategory:
+  *       type: object
+  *       properties:
+  *         id:
+  *           type: string
+  *         name:
+  *           type: string
+  *     Category:
+  *       type: object
+  *       properties:
+  *         id:
+  *           type: string
+  *         name:
+  *           type: string
+  *         subCategories:
+  *           type: array
+  *           items:
+  *             $ref: '#/components/schemas/SubCategory'
+  * /categories:
+  *   get:
+  *     tags: [categories]
+  *     description: Gets all categories
+  *     responses:
+  *       200:
+  *         content:
+  *           application/json:
+  *             schema:
+  *               type: array
+  *               items:
+  *                 $ref: '#/components/schemas/Category'
+*/
 module.exports = (server) => {	
 	return router;
 };
